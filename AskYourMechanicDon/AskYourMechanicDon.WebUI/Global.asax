3bfd0e36b9ba7b56632bee378b5218f56ccd1f75
William E Morgan <bmorgan@telusplanet.net>
2017-11-16 08:29:03 -0700

    Initial setup

---
+++ AskYourMechanicDon/AskYourMechanicDon.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AskYourMechanicDon.WebUI.MvcApplication" Language="C#" %>
---
+++ AskYourMechanicDon/AskYourMechanicDon.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AskYourMechanicDon.WebUI.Startup))]
+namespace AskYourMechanicDon.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
