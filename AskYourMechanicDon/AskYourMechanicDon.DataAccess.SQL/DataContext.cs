4390c8c25cfd7ca81b9e97eaab63c7e2ab3f1cbf
William E Morgan <bmorgan@telusplanet.net>
2017-11-17 08:08:45 -0700

    EntityFramework

---
+++ AskYourMechanicDon/AskYourMechanicDon.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AskYourMechanicDon.Core.Models;
+
+namespace AskYourMechanicDon.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
