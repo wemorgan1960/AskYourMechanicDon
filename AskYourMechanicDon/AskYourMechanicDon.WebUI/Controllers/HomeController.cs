2f92e450f7845aa4a8ea273da877025c29291d55
William E Morgan <bmorgan@telusplanet.net>
2017-11-18 10:54:35 -0700

    Add Products page

--- AskYourMechanicDon/AskYourMechanicDon.WebUI/Controllers/HomeController.cs
+++ AskYourMechanicDon/AskYourMechanicDon.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AskYourMechanicDon.Core.Contracts;
+using AskYourMechanicDon.Core.Models;
 
 namespace AskYourMechanicDon.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
+            List<Product> products = context.Collection().ToList();
             return View();
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+
         }
 
         public ActionResult About()
