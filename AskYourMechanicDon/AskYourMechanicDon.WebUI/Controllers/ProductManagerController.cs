51301a961a97229e2db5a28fa6afffed51c707f2
William E Morgan <bmorgan@telusplanet.net>
2017-11-18 07:02:09 -0700

    Add image functionality

--- AskYourMechanicDon/AskYourMechanicDon.WebUI/Controllers/ProductManagerController.cs
+++ AskYourMechanicDon/AskYourMechanicDon.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -36,7 +37,7 @@ namespace AskYourMechanicDon.WebUI.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if(!ModelState.IsValid)
             {
@@ -45,6 +46,11 @@ namespace AskYourMechanicDon.WebUI.Controllers
             }
             else
             {
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
                 context.Insert(product);
                 context.Commit();
 
@@ -68,7 +74,7 @@ namespace AskYourMechanicDon.WebUI.Controllers
             }
         }
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (product == null)
@@ -81,13 +87,18 @@ namespace AskYourMechanicDon.WebUI.Controllers
                 {
                     return View(product);
                 }
+
+                if (file != null)
+                {
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Context//ProductImages//") + productToEdit.Image);
+                }
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
                 productToEdit.Question = product.Question;
                 productToEdit.VIN = product.VIN;
-                productToEdit.Image = product.Image;
 
                 context.Commit();
 
